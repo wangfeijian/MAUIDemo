17acada66825087a6d05880881c836c7a387881d
王飞箭 <wangfeijianhao@163.com>
2022-07-07 15:54:54 +0800

    Finished the first MaUI app”

---
+++ FirstMaui/FirstMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ FirstMaui/FirstMaui/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿namespace FirstMaui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	string translateNumber;
+
+	private void OnTranslate(object sender, EventArgs e)
+	{
+		string enteredNumber = PhoneNumberText.Text;
+		translateNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+		if (!string.IsNullOrEmpty(translateNumber))
+		{
+			CallButton.IsEnabled = true;
+			CallButton.Text = "Call" + translateNumber;
+		}
+		else
+		{
+			CallButton.IsEnabled = false;
+			CallButton.Text = "Call";
+		}
+	}
+
+	async void OnCall(object sender, EventArgs e)
+	{
+		if(await DisplayAlert("Dial a Number", "Would you like to call " + translateNumber +　"?", "Yes","No"))
+		{
+			try
+			{
+				PhoneDialer.Default.Open(translateNumber);
+			}
+			catch (ArgumentException)
+			{
+				await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+			}
+			catch(FeatureNotSupportedException)
+			{
+				await DisplayAlert("Unable to dial", "Phone dialing not supported.", "OK");
+			}
+			catch(Exception)
+			{
+				await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+			}
+		}
+	}
+}
+
